1e6c423593f36a3a27b00be8c7e66df3cecb2a9b
Aminprvz <68307911+Aminprvz@users.noreply.github.com>
2020-07-16 14:02:01 -0700

    Update mainwindow.h

--- 2d-graphics-modeler/mainwindow.h
+++ 2d-graphics-modeler/mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTime>
+#include <QMessageBox>
+#include "renderarea.h"
+#include "adminlogin.h"
+#include "contact.h"
+#include "palette.h"
+#include "shapeinfo.h"
 
 namespace Ui {
 class MainWindow;
@@ -14,9 +21,41 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    ShapeBuffer buffer;
+    RenderArea *renderArea;
+    QTime timeDifference (const QTime&, const QTime&);
+
+private slots:
+    void on_actionLogin_triggered();
+    void on_actionContact_Us_triggered();
+    void on_actionSave_triggered();
+    void on_actionOpen_triggered();
+    void on_actionQuit_triggered();
+
+    void on_actionShow_Info_triggered();
+    
+    void on_actionOpen_palette_triggered();
+
+    void on_moveButton_clicked();
+
+    void on_actionShow_Area_triggered();
+
+    void on_actionShow_Perimeter_triggered();
+
+    void on_actionLog_Out_triggered();
+
+protected:
+    void timerEvent (QTimerEvent* event);
 
 private:
     Ui::MainWindow *ui;
+    adminLogin *admin;
+    Contact    *contact;
+    Palette    *palette;
+    ShapeInfo *shapeInfo;
+    QTime initialTime;
 };
+
+
 
 #endif // MAINWINDOW_H
