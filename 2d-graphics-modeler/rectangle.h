2879decae6dc768cdd61696bcd1c35ff0b86666e
r1gg3y <66648496+r1gg3y@users.noreply.github.com>
2020-07-20 03:01:27 -0700

    Update rectangle.h

--- 2d-graphics-modeler/rectangle.h
+++ 2d-graphics-modeler/rectangle.h
@@ -1,11 +1,24 @@
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
 
+#include "shape.h"
 
-class rectangle
+class Rectangle : public Shape
 {
+private:
+    int l; //length
+    int w; //width
+    int x;
+    int y;
 public:
-    rectangle();
+    void draw();
+    void move(const int, const int);
+    double area() const;
+    double perimeter() const;
+    // constructors
+    Rectangle();
+    Rectangle(int, int, int, int);
+    Rectangle(ShapeBuffer);
 };
 
 #endif // RECTANGLE_H
