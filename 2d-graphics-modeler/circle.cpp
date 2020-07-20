d72e83b15bb187ea4da061eb550a31a5bdd3afd6
Aminprvz <68307911+Aminprvz@users.noreply.github.com>
2020-07-20 15:27:39 -0700

    Update circle.cpp

--- 2d-graphics-modeler/circle.cpp
+++ 2d-graphics-modeler/circle.cpp
@@ -1,6 +1,39 @@
 #include "circle.h"
+#include <QtMath>
 
-circle::circle()
+Circle::Circle(const ShapeBuffer& arg): Ellipse(arg)
 {
-
+    radius2 = radius1;
 }
+
+void Circle::setShapeBuffer(ShapeBuffer &temp)
+{
+   Ellipse::setShapeBuffer(temp);
+}
+
+void Circle::draw(const int x, const int y)
+{
+    Ellipse::draw(x,y);
+}
+
+void Circle::draw()
+{
+    Ellipse::draw();
+}
+
+void Circle::move(const int x, const int y)
+{
+    Ellipse::move(x,y);
+}
+
+double Circle::perimeter() const
+{
+    return 2 * M_PI * radius1;
+}
+
+double Circle::area() const
+{
+    return M_PI * radius1 * radius1;
+}
+
+
