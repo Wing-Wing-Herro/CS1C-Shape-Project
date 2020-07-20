2c420ad2d7c07d8a1f66d18b6327fb73ab6c71ff
Dogtor-Hegcht <68541916+Dogtor-Hegcht@users.noreply.github.com>
2020-07-20 03:21:20 -0700

    Update square.h

--- 2d-graphics-modeler/square.h
+++ 2d-graphics-modeler/square.h
@@ -1,11 +1,23 @@
 #ifndef SQUARE_H
 #define SQUARE_H
+#include "shape.h"
 
-
-class square
+class Square : public Shape
 {
+private:
+    int side;
+    int x;
+    int y;
 public:
-    square();
+    Square();
+    Square(ShapeBuffer s);
+    virtual void draw(const int x, const int y);
+    virtual void draw();
+    virtual void move(const int x,const int y);
+    virtual double area() const;
+    virtual double perimeter() const;
+    virtual int getX() const;
+    virtual int getY() const;
 };
 
 #endif // SQUARE_H
