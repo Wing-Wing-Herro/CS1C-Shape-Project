3a4a8c644b262e95f543f2acff81fc373ae26208
r1gg3y <66648496+r1gg3y@users.noreply.github.com>
2020-07-20 02:29:58 -0700

    Update line.h

--- 2d-graphics-modeler/line.h
+++ 2d-graphics-modeler/line.h
@@ -1,11 +1,25 @@
 #ifndef LINE_H
 #define LINE_H
 
+#include "shape.h"
 
-class line
+
+class Line : public Shape
 {
+private:
+    int x1;
+    int x2;
+    int y1;
+    int y2;
 public:
-    line();
+    void draw();
+    void move(const int, const int);
+    double area() const;
+    double perimeter() const;
+    Line();
+    Line(int,int,int,int);
+    Line(ShapeBuffer);
+    ~Line();
 };
 
 #endif // LINE_H
